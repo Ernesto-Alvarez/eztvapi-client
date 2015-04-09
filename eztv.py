48882b29e8f544efd312e55ddca86f6c4d146101
Ernesto-Alvarez <ernesto.alvarez@gmail.com>
2015-04-09 08:52:59 -0300

    Version 0.3.0 (not working properly)

--- eztv.py
+++ eztv.py
@@ -2,13 +2,36 @@ import requests
 import copy
 import magnet
 import videocollection
+import threading
 
 class index():
-	def __init__(self,url = 'http://eztvapi.re/'):
+	def __init__(self,url = 'http://eztvapi.re/',retry=True):
 		self.base_url = url
 		self.show_list = {}
 		self.missing_pages = []
 		self.get_all_show_pages()
+		self.page_maintenance = None
+		self.set_auto_retry(retry)
+
+
+	def set_auto_retry(self,retry):
+		#DOES NOT WORK. 
+		#Object will permanently remain in scope if auto retry is active and all references are lost.
+		#Timer will hold a reference of its own and will run forever, causing a memory (and thread) leak.
+		if retry == True:
+			if self.page_maintenance == None:
+				self.page_maintenance = threading.Timer(15,self.__auto_retry_pages)
+				self.page_maintenance.daemon = True
+				self.page_maintenance.start()
+		else:
+			if self.page_maintenance != None:
+				self.page_maintenance.cancel()
+				self.page_maintenance = None
+
+	def __auto_retry_pages(self):
+		self.get_missing_pages()
+		self.page_maintenance = threading.Timer(60,self.__auto_retry_pages)
+		self.page_maintenance.start()
 
 	def get_all_show_pages(self):	
 		pages = requests.get(self.base_url + 'shows',proxies={'http': 'http://127.0.0.1:8080'})
@@ -19,9 +42,9 @@ class index():
 
 	def get_missing_pages(self):
 		for i in copy.copy(self.missing_pages):
-			self.get_show_page(i)
+			self.__get_show_page(i)
 
-	def get_show_page(self,page):
+	def __get_show_page(self,page):
 		print "Getting: " + page
 		shows = requests.get(self.base_url + page,proxies={'http': 'http://127.0.0.1:8080'})
 		try:
